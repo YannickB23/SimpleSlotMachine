93afc07c18b3903ea7a329cad2b145b08b92f112
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-01-21 11:42:47 +0100

    Add project files.

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace SlotMachine
+{
+    partial class frmSlot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSlot));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.lblSlotMachine = new System.Windows.Forms.Label();
+            this.imgFruit = new System.Windows.Forms.ImageList(this.components);
+            this.txtAmountInserted = new System.Windows.Forms.TextBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic1.Location = new System.Drawing.Point(10, 108);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(165, 165);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic2.Location = new System.Drawing.Point(181, 108);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(165, 165);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic3.Location = new System.Drawing.Point(352, 108);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(165, 165);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            // 
+            // lblSlotMachine
+            // 
+            this.lblSlotMachine.Font = new System.Drawing.Font("MV Boli", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSlotMachine.ForeColor = System.Drawing.Color.Gold;
+            this.lblSlotMachine.Location = new System.Drawing.Point(10, 22);
+            this.lblSlotMachine.Name = "lblSlotMachine";
+            this.lblSlotMachine.Size = new System.Drawing.Size(507, 75);
+            this.lblSlotMachine.TabIndex = 3;
+            this.lblSlotMachine.Text = "Slot Machine";
+            this.lblSlotMachine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgFruit
+            // 
+            this.imgFruit.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgFruit.ImageStream")));
+            this.imgFruit.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgFruit.Images.SetKeyName(0, "banana.png");
+            this.imgFruit.Images.SetKeyName(1, "lightning.jpg");
+            this.imgFruit.Images.SetKeyName(2, "cherrycherry.png");
+            // 
+            // txtAmountInserted
+            // 
+            this.txtAmountInserted.Location = new System.Drawing.Point(251, 318);
+            this.txtAmountInserted.Name = "txtAmountInserted";
+            this.txtAmountInserted.Size = new System.Drawing.Size(165, 22);
+            this.txtAmountInserted.TabIndex = 4;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.ForeColor = System.Drawing.Color.Gold;
+            this.lblAmount.Location = new System.Drawing.Point(10, 314);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(203, 26);
+            this.lblAmount.TabIndex = 5;
+            this.lblAmount.Text = "Amount to play =";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.Green;
+            this.btnPlay.Location = new System.Drawing.Point(10, 402);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(259, 51);
+            this.btnPlay.TabIndex = 6;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Green;
+            this.btnExit.Location = new System.Drawing.Point(275, 402);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(242, 51);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmSlot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(530, 475);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.txtAmountInserted);
+            this.Controls.Add(this.lblSlotMachine);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmSlot";
+            this.Text = "Slot machine";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.Label lblSlotMachine;
+        private System.Windows.Forms.ImageList imgFruit;
+        private System.Windows.Forms.TextBox txtAmountInserted;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class frmSlot : Form
+    {
+        public frmSlot()
+        {
+            InitializeComponent();
+        }
+        decimal totalInserted = 0.00m;
+        decimal totalWon = 0.00m;
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            decimal amountWon = 0.00m;
+            decimal amountInserted = 0.00m;
+
+            if (decimal.TryParse(txtAmountInserted.Text, out amountInserted) && amountInserted > 0)
+            {
+            Random rnd = new Random();
+            int pic_1 = rnd.Next(0, 3);
+            int pic_2 = rnd.Next(0, 3);
+            int pic_3 = rnd.Next(0, 3);
+
+            pic1.Image = imgFruit.Images[pic_1];
+            pic2.Image = imgFruit.Images[pic_2];
+            pic3.Image = imgFruit.Images[pic_3];
+
+                totalInserted += amountInserted;
+                totalWon += amountWon = GetWinningAmount(pic_1, pic_2, pic_3, amountInserted);
+            }
+        }
+        private decimal GetWinningAmount(int p1, int p2, int p3, decimal amountInserted)
+        {
+            if (p1 == p2 && p2 == p3)
+            {
+                return amountInserted * 3;
+            }
+            else if (p1 == p2 || p2 == p3 || p1 == p3)
+            {
+                return amountInserted * 2;
+            }
+            return -amountInserted;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Total inserted = {totalInserted}".ToString() + 
+                            $"\nTotal won = {totalWon}".ToString());
+        }
+    }
+}
